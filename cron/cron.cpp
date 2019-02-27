2f718a50fcdce71aae27f9e65aad6e601651792b
Emil Guseynov <theluckyemil@gmail.com>
2019-02-27 16:25:14 +0300

    Payment logic

--- cron/cron.cpp
+++ cron/cron.cpp
@@ -1,3 +1,4 @@
+#include <eosiolib/asset.hpp>
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/transaction.hpp>
 #include <eosiolib/singleton.hpp>
@@ -24,17 +25,20 @@ public:
     }
 
     ACTION start(name from) {
+        require_auth(get_self());
         stop_execution.set(false, from);
         print("Set START");
     }
 
     ACTION stop(name from) {
+        require_auth(get_self());
         stop_execution.set(true, from);
         print("Set STOP");
     }
 
     ACTION schedule(name from, name account, string action, uint32_t period) {
         require_auth(from);
+        deposit(from, CALL_PRICE * PREPAY_CALL_AMOUNT);
         time_point_sec current_time(now());
         timetable cron_table(_code, _code.value);
         const auto pk = cron_table.available_primary_key();
@@ -56,7 +60,7 @@ public:
         if (stop_execution.get())
             return;
 
-        scan_table(rows_count);
+        scan_schedules(rows_count);
         create_transaction(_self, _self, "run", polling_interval, make_tuple(_self, polling_interval, rows_count));
     }
 
@@ -66,6 +70,32 @@ public:
 
     ACTION disable(name from, uint64_t job_id) {
         change_status(from, job_id, false);
+    }
+
+    ACTION deposit(name from, uint64_t amount) {
+        require_auth(from);
+        action(
+                permission_level(from, "active"_n),
+                "eosio.token"_n,
+                "transfer"_n,
+                std::make_tuple(from, _self, asset(amount,
+                                                   symbol("CRON", 0)), std::string("cronos_deposit"))
+        ).send();
+        add_balance(from, amount);
+    }
+
+    ACTION withdraw(name from, uint64_t amount) {
+        require_auth(from);
+        check(get_balance(from) >= amount, "Withdraw amount is larger than wallet balance");
+        reduce_balance(from, amount);
+        action(
+                permission_level(_self, "active"_n),
+                "eosio.token"_n,
+                "transfer"_n,
+                std::make_tuple(_self, from, asset(amount,
+                                                   symbol("CRON", 0)), std::string("cronos_withraw"))
+        ).send();
+
     }
 
     cron(name receiver, name code, datastream<const char*> ds):
@@ -80,7 +110,7 @@ public:
         }
     }
 
-    void scan_table(uint32_t rows_count) {
+    void scan_schedules(uint32_t rows_count) {
         print("Scanning timetable\n");
         timetable cron_table(_code, _code.value);
         for (const auto& item : cron_table.get_index<"lastupdated"_n>()) {
@@ -89,12 +119,24 @@ public:
             }
             print("Processing ", item.key, "\n");
             time_point_sec current_time(now());
+            cron_table.modify(item, _code, [&](auto& row) {
+                row.last_updated = current_time;
+            });
+
+            if (!item.active) {
+                continue;
+            }
+
             if (current_time >= item.next_run) {
-                // Empty Args
-                create_transaction(item.from, item.account, item.action, item.period, tuple<name>(item.from));
+                const name& account_from = item.from;
+                reduce_balance(account_from, CALL_PRICE);
+
+                if (get_balance(account_from) >= CALL_PRICE) {
+                    create_transaction(account_from, item.account, item.action, item.period, tuple<name>(account_from));
+                }
+
                 cron_table.modify(item, _code, [&](auto& row) {
                     row.next_run = item.next_run + item.period;
-                    row.last_updated = current_time;
                 });
             }
             --rows_count;
@@ -115,19 +157,44 @@ public:
         t.delay_sec = delay;
 
         auto sender_id = unique_id.get();
-        t.send(sender_id, payer);
+        t.send(sender_id, _code);
         print("Scheduled with a delay of ", delay, "\n");
         unique_id.set(sender_id + 1, _code);
     }
 
     void change_status(name from, uint64_t job_id, bool active) {
         timetable cron_table(_code, _code.value);
-        auto iterator = cron_table.find(job_id);
-        if (iterator != cron_table.end()) {
-            cron_table.modify(iterator, from, [&]( auto& row ) {
-                row.active = active;
+        cron_table.modify(cron_table.require_find(job_id), from, [&]( auto& row ) {
+            row.active = active;
+        });
+    }
+
+    void update_balance(name from, int64_t amount) {
+        balance_sheet balance(_code, _code.value);
+        auto iterator = balance.find(from.value);
+        if (iterator != balance.end()) {
+            balance.modify(iterator, from, [&](auto& row) {
+                row.balance += amount;
+            });
+        } else {
+            balance.emplace(from, [&](auto& row) {
+                row.account = from;
+                row.balance = amount;
             });
         }
+    }
+
+    void add_balance(name from, uint64_t amount) {
+        update_balance(from, amount);
+    }
+
+    void reduce_balance(name from, uint64_t amount) {
+        update_balance(from, -1 * static_cast<int64_t>(amount));
+    }
+
+    uint64_t get_balance(name from) {
+        balance_sheet balance(_self, _self.value);
+        return balance.require_find(from.value)->balance;
     }
 
 private:
@@ -145,14 +212,27 @@ private:
         uint64_t by_last_updated() const { return last_updated.utc_seconds; }
     };
 
+    TABLE balance_data {
+        name account;
+        uint64_t balance;
+
+        uint64_t primary_key() const { return account.value; }
+    };
+
     typedef eosio::multi_index<"timetable"_n,
                                timetable_data,
                                eosio::indexed_by<"lastupdated"_n,
                                                  eosio::const_mem_fun<timetable_data,
                                                                       uint64_t,
                                                                       &timetable_data::by_last_updated>>> timetable;
+
+    typedef eosio::multi_index<"balance"_n, balance_data> balance_sheet;
+
     eosio::singleton<"flag"_n, bool> stop_execution;
     eosio::singleton<"increment"_n, uint64_t> unique_id;
+
+    const uint32_t PREPAY_CALL_AMOUNT = 5;
+    const uint32_t CALL_PRICE = 10;
 };
 
-EOSIO_DISPATCH(cron, (dumb)(start)(stop)(schedule)(run))
+EOSIO_DISPATCH(cron, (dumb)(enable)(disable)(deposit)(withdraw)(start)(stop)(schedule)(run))
